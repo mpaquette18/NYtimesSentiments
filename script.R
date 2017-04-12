83df6daf74469a2c12642b4cf9454a05ab1c1479
Michael Paquette <mpaquette18@cornellcollege.edu>
2017-04-11 21:08:59 -0500

    Update R script
    updated R script that makes sentiment data from headlines

--- script.R
+++ script.R
@@ -2,18 +2,22 @@ library(RCurl)
 library(RSentiment)
 library(stringr)
 
+#read in csv data
 dataFrame <- data.frame(read.csv("us.csv"))
 worldData <- data.frame(read.csv("world.csv"))
  
-##newData <- calculate_sentiment(dataFrame[1:3000,1])
+#replace all special character for use with calculate sentiment
 dataFrame$title <- str_replace_all(dataFrame$title, "[^[:alnum:]]", " ") #removing all special characters
 worldData$title <- str_replace_all(worldData$title, "[^[:alnum:]]", " ")
+
+#calculate sentiments of headlines and store them in new dataframe
 dataFrame3 <-  calculate_sentiment(dataFrame$title)
-write.csv(dataFrame3, file = "sentiment_title.csv")
+dataframe4 <- calculate_sentiment(worldData$title)
 
+#output new data frames as csv
+write.csv(dataFrame3, file = "us_sentiment_title.csv")
+write.csv(dataframe4, file="world_sentiment_title.csv")
 
-pooop <- dataFrame$date() 
+#plot the headline sentiment data
 plot(dataFrame3$sentiment)
-##dataFrame$sentiment_title <-  calculate_sentiment(dataFrame$title)
-#dataFrame$sentiment_title.text <- null
-#plot(dataFrame$sentiment_title)
+plot(dataframe4$sentiment)
